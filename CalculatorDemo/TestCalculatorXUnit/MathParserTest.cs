658d10b7a9cd18d4df96fcce80bdd73f5c2c20c9
Jamal Idb. <jamal.id.901@gmail.com>
2025-03-31 16:51:22 -0400

    add more test cases for MathParser

--- CalculatorDemo/TestCalculatorXUnit/MathParserTest.cs
+++ CalculatorDemo/TestCalculatorXUnit/MathParserTest.cs
@@ -4,8 +4,24 @@ namespace TestCalculatorXUnit;
 
 public class MathParserTest
 {
+    [Fact]
+    public void InvalidInput_TestCase() //null or empty case
+    {
+        IMathParser parser = new MathParser();
+        Assert.Throws<ArgumentException>(() => parser.Normalise(null));
+        Assert.Throws<ArgumentException>(() => parser.Normalise(""));
+    }
+
+    [Fact]
+    public void InputDoesntMatch_TestCase() //input doesnt match regex
+    {
+        IMathParser parser = new MathParser();
+        Assert.Throws<ArgumentException>(() => parser.Normalise("a@a"));
+    }
+
     [Theory]
     [InlineData("2+1", new string[] { "2", "+", "1" })]
+    [InlineData("60-(3*10)", new string[] { "60", "-", "(", "3", "*", "10", ")" })]
     public void TestCases(string input, string[] expected)
     {
         IMathParser parser = new MathParser();
